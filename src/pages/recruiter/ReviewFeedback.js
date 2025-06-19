e85d96ce0f2fe8c8349c2e105c58a129225e522a
Diushan <forum_st03@forum.ac.jp>
2025-06-19 15:53:23 +0900

    reviewfeedback.js

--- src/pages/recruiter/ReviewFeedback.js
+++ src/pages/recruiter/ReviewFeedback.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import '../../styles/recruiter.css';
 
 import axios from "axios";
